37b675d31d2d08823aa11dd4851e752bbdcf02be
Marian Serna <marianhalliday@gmail.com>
2017-05-08 17:17:45 -0400

    Building scene

--- src/Scene.js
+++ src/Scene.js
@@ -5,10 +5,11 @@ require('three/examples/js/controls/OrbitControls.js');
 export default class Scene {
   constructor(container) {
     this.container = container;
+    this.group = new THREE.Group();
 
     this.init();
     this.addLights();
-    this.addSphere();
+    this.addSpoke();
     this.loop();
 
     window.addEventListener('resize', () => this.handleResize());
@@ -43,11 +44,37 @@ export default class Scene {
     this.scene.add(light);
   }
 
-  addSphere() {
-    const geometry = new THREE.SphereBufferGeometry( 5, 32, 32 );
-    const material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-    const sphere = new THREE.Mesh( geometry, material );
-    this.scene.add( sphere );
+  addSpoke() {
+    for (let i = 0; i <= 50; i++) {
+      setTimeout(() => {
+        const anchor = new THREE.Object3D();
+
+        const lineMaterial = new THREE.LineBasicMaterial({color: 0x8C95AA, opacity: 0.7});
+        const lineGeometry = new THREE.Geometry();
+        lineGeometry.vertices.push(
+          new THREE.Vector3(0, 0, 0),
+        	new THREE.Vector3(30, 0, 0)
+        );
+        const line = new THREE.Line(lineGeometry, lineMaterial);
+        anchor.add(line);
+
+        const circleGeometry = new THREE.CircleGeometry(0.3, 64);
+        const circleMaterial = new THREE.MeshBasicMaterial({color: 0xC7AFBD, opacity: 0.8});
+        const circle = new THREE.Mesh(circleGeometry, circleMaterial);
+        circle.position.x += 10;
+        anchor.add(circle);
+
+        const circleTwoGeometry = new THREE.CircleGeometry(0.7, 64);
+        const circleTwoMaterial = new THREE.MeshBasicMaterial({color: 0xF16B6F, opacity: 0.8});
+        const circleTwo = new THREE.Mesh(circleTwoGeometry, circleTwoMaterial);
+        circleTwo.position.x += 18;
+        anchor.add(circleTwo);
+
+        anchor.rotation.z += 0.125664 * i;
+
+        this.scene.add(anchor);
+      }, 20 * i);
+    }
   }
 
   handleResize() {
