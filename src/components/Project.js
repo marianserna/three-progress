ab6ce00700ff75c87a8c16d39e817f3444f45899
Marian Serna <marianhalliday@gmail.com>
2017-05-07 20:26:30 -0400

    Setting up components & React Router

---
+++ src/components/Project.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Project extends React.Component {
+  render() {
+    return (
+      <div className="Project">
+        Welcome to React!
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import Main from './components/Main';
+import Project from './components/Project';
+import './css/styles.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Root = function() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={Main} />
+        <Route path="/project/:id" component={Project} />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
